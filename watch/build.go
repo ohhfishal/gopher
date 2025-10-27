a88978f828303d9b26e2e75e775e191565acfa2d
ohhfishal <ohhfishal@gmail.com>
2025-10-27 11:06:37 -0700

    feat: basic go build call

--- watch/build.go
+++ watch/build.go
@@ -1,9 +1,16 @@
 package watch
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
+	"os/exec"
 )
+
+type BuildFunc func(context.Context, string) ([]BuildEvent, error)
 
 // From https://pkg.go.dev/cmd/go#hdr-Build__json_encoding
 type BuildEvent struct {
@@ -17,7 +24,44 @@ type BuildEvent struct {
 	// build-fail - The build failed
 }
 
-func Build(ctx context.Context, logger *slog.Logger) error {
-	logger.Info("building")
-	return nil
+func (config CMD) Build(ctx context.Context, logger *slog.Logger, build BuildFunc) {
+	events, err := build(ctx, config.Path)
+	if err != nil {
+		logger.Error("go build", "err", err)
+		return
+	}
+	logger.Info("built", "events", events)
+
+	// Parse the output from JSON
+	// Print pretty
+	// Repeat with other steps
+}
+
+func goBuild(ctx context.Context, path string) ([]BuildEvent, error) {
+	// TODO: Provide options to add more flags
+	cmd := exec.CommandContext(ctx, "go", "build", "-json")
+	cmd.Dir = path
+
+	output, err := cmd.Output()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok && exitErr.ExitCode() != 1 {
+			return nil, fmt.Errorf("running command: %w: %s", err, string(output))
+		}
+	}
+	if err == nil || len(output) == 0 {
+		return []BuildEvent{}, nil
+	}
+
+	var events []BuildEvent
+	decoder := json.NewDecoder(bytes.NewReader(output))
+	for {
+		var event BuildEvent
+		if err := decoder.Decode(&event); err == io.EOF {
+			// TODO: Confirm the err is okay
+			return events, nil
+		} else if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
 }
--- watch/watch.go
+++ watch/watch.go
@@ -12,7 +12,7 @@ import (
 )
 
 type CMD struct {
-	Paths    []string      `arg:"" default:"." name:"path" help:"Paths to watch (default=${default})" type:"path"`
+	Path     string        `arg:"" default:"." name:"path" help:"Paths to watch (default=${default})" type:"path"`
 	Interval time.Duration `default:"3s" help:""`
 }
 
@@ -21,36 +21,35 @@ func (config *CMD) Run(ctx context.Context, stdout io.Writer, logger *slog.Logge
 	if err != nil {
 		return err
 	}
-	defer watcher.Close()
+	defer watcher.Close() //nolint: errcheck
 
-	for _, path := range config.Paths {
-		if err := watcher.Add(path); err != nil {
-			return fmt.Errorf("adding path to watch list: %w", err)
-		}
-		if err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-			if d.IsDir() {
-				watcher.Add(path)
+	if err := watcher.Add(config.Path); err != nil {
+		return fmt.Errorf("adding path to watch list: %w", err)
+	}
+	if err := filepath.WalkDir(config.Path, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			if err := watcher.Add(path); err != nil {
+				return fmt.Errorf("adding path to watch list: %w", err)
 			}
-			return nil
-		}); err != nil {
-			return fmt.Errorf("walking directories: %w", err)
 		}
+		return nil
+	}); err != nil {
+		return fmt.Errorf("walking directories: %w", err)
 	}
 
 	go config.Work(ctx, watcher, logger)
 
-	select {
-	case <-ctx.Done(): // Context was canceled or deadline expired
-		logger.Info("done", "reason", ctx.Err())
-		return nil
-	}
+	<-ctx.Done()
+	logger.Info("done", "reason", ctx.Err())
+	return nil
 }
 
 func (config CMD) Work(ctx context.Context, watcher *fsnotify.Watcher, logger *slog.Logger) {
 	refresh := time.NewTicker(config.Interval / 10)
 	defer refresh.Stop()
 
-	Build(ctx, logger)
+	// TODO: Control this dependency injection somewhere
+	config.Build(ctx, logger, goBuild)
 	lastBuild := time.Now()
 	build := false
 
@@ -58,9 +57,10 @@ func (config CMD) Work(ctx context.Context, watcher *fsnotify.Watcher, logger *s
 		select {
 		case <-ctx.Done():
 			logger.Info("closing")
+			return
 		case <-refresh.C:
 			if build && time.Since(lastBuild) > config.Interval {
-				Build(ctx, logger)
+				config.Build(ctx, logger, goBuild)
 				lastBuild = time.Now()
 				build = false
 			}
