8ea42604cbb391ca17fbcc9ebb4ff5a1a62d387e
ohhfishal <ohhfishal@gmail.com>
2025-12-13 14:38:41 -0700

    chore: add todo

--- gopher.go
+++ gopher.go
@@ -11,12 +11,23 @@ import (
 	"time"
 )
 
+// TODO: Maybe create a Cached Runner, so they can make changes.
+//       That way go fmt doesn't trigger another build?
+// func CacheExampel(ctx context.Context, args RunArgs) error {
+// 	return Run(ctx, NowAnd(Every(3*time.Second)),
+//		Cached(
+// 			&GoBuild{
+// 				Output: "target/dev",
+// 			},
+// 			&GoFormat{},
+//    )
+// 	)
+// }
+
 // Devel builds the app as you make changes.
 func Devel(ctx context.Context, args RunArgs) error {
 	return Run(ctx, NowAnd(Every(3*time.Second)),
-		&FileCache{
-			Path: "",
-		},
+		&FileCache{},
 		&GoBuild{
 			Output: "target/dev",
 		},
