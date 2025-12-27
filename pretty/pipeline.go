a0f58a5a45b54fa57ca0ac2048f21f8a9711c94b
ohhfishal <ohhfishal@gmail.com>
2025-12-26 19:20:23 -0700

    fix: pipeline to not make assumptions abot write input

--- pretty/pipeline.go
+++ pretty/pipeline.go
@@ -2,7 +2,7 @@ package pretty
 
 import (
 	"io"
-	"slices"
+	"strings"
 )
 
 var _ io.Writer = &Pipeline{}
@@ -22,15 +22,36 @@ func NewPipeline(stdout io.Writer, delim string) *Pipeline {
 }
 
 func (pipeline *Pipeline) Write(content []byte) (int, error) {
-	if !pipeline.inline {
+	if len(content) == 0 {
+		return 0, nil
+	} else if !pipeline.inline {
 		if _, err := io.WriteString(pipeline.stdout, pipeline.delim); err != nil {
 			return 0, err
 		}
 		pipeline.inline = true
 	}
 
-	if slices.Contains(content, '\n') {
+	contentStr := string(content)
+	index := strings.Index(contentStr, "\n")
+	if index == -1 {
+		// No newline
+		pipeline.inline = true
+		return pipeline.stdout.Write(content)
+	} else if index == len(contentStr)-1 {
+		// Single newline at the end
 		pipeline.inline = false
+		return pipeline.stdout.Write(content)
 	}
-	return pipeline.stdout.Write(content)
+	// Write the first line then recurse
+	left, right := contentStr[:index], contentStr[index:]
+	count, err := io.WriteString(pipeline.stdout, left)
+	if err != nil {
+		return count, err
+	}
+	pipeline.inline = false
+	delta, err := pipeline.Write([]byte(right))
+	if err != nil {
+		return count, err
+	}
+	return count + delta, nil
 }
