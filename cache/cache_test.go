6af653db772475fa64a4b0bb4ea8af1d76ab2861
ohhfishal <ohhfishal@gmail.com>
2026-01-09 15:04:18 -0700

    feat: context that cancels when a file changes

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,9 +1,14 @@
 package cache_test
 
 import (
+	"context"
+	"errors"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/ohhfishal/gopher/cache"
+	"github.com/ohhfishal/nibbles/assert"
 )
 
 func TestHashDeterministic(t *testing.T) {
@@ -14,4 +19,34 @@ func TestHashDeterministic(t *testing.T) {
 	if hash1 != hash2 {
 		t.Errorf("%s != %s", hash1, hash2)
 	}
+}
+
+func TestContext(t *testing.T) {
+	assert := assert.With(t)
+	dir := t.TempDir()
+
+	tempFile, err := os.CreateTemp(dir, "test_context-*.txt")
+	assert.Nil(err)
+	assert.Nil(tempFile.Close())
+
+	t.Logf("file: %s", tempFile.Name())
+	ctx := cache.WithFileCancel(t.Context(), tempFile.Name())
+	assert.Nil(ctx.Err())
+
+	// Give the goroutine time to work
+	time.Sleep(250 * time.Millisecond)
+	assert.Nil(os.WriteFile(tempFile.Name(), []byte("Hello world"), 0777))
+	time.Sleep(250 * time.Millisecond)
+
+	select {
+	case <-ctx.Done():
+	default:
+		assert.Unreachable("context is not done")
+	}
+
+	cause := context.Cause(ctx)
+	assert.True(
+		errors.Is(cause, cache.ErrFileChanged),
+		"%v == nil", cause,
+	)
 }
---
+++ cache/context.go
@@ -0,0 +1,60 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"github.com/fsnotify/fsnotify"
+	"github.com/ohhfishal/nibbles/assert"
+	"log/slog"
+)
+
+var ErrFileChanged = errors.New("file changed")
+
+// Returns a context that gets cancels using [ErrFileChanged] when the file is chcanged.
+func WithFileCancel(ctx context.Context, file string) context.Context {
+	newCtx, cancel := context.WithCancelCause(ctx)
+
+	// TODO: This goroutine might be leaking
+	go func() {
+		assert.Nil(newCtx.Err())
+
+		err := watch(newCtx, file)
+		cancel(err)
+
+		select {
+		case <-newCtx.Done():
+			return
+		default:
+			assert.Unreachable("context not canceled despite call")
+		}
+	}()
+	return newCtx
+}
+
+func watch(ctx context.Context, file string) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	if err := watcher.Add(file); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case event := <-watcher.Events:
+			if !event.Has(fsnotify.Write) && !event.Has(fsnotify.Create) && !event.Has(fsnotify.Rename) {
+				continue
+			}
+			slog.Info("file changed returning error", "event", event)
+			return ErrFileChanged
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				continue
+			}
+			return err
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
